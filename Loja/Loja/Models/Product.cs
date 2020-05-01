e28e7f5bd84356c6d91b25d921ff4a534a643234
Francisco Pereira <aluno21369@ipt.pt>
2020-05-01 15:36:28 +0100

    criacao das class dos products e users

---
+++ Loja/Loja/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public string item { get; set; }
+
+        public int stock { get; set; }
+
+        public string description { get; set; }
+
+    }
+}
---
+++ Loja/Loja/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Loja.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+    
+        public string role { get; set; }
+        
+        public string nome { get; set; }
+
+        public string email { get; set; }
+
+        public string address { get; set; }
+
+        public DateTime Inc_date { get; set; }
+    }
+}
