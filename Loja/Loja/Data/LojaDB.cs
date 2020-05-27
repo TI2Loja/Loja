c47a157a56bb82cae0f850d433291af1bdca1922
Francisco Pereira <aluno21369@ipt.pt>
2020-05-27 15:02:12 +0100

    Add LojaDB

---
+++ Loja/Loja/Data/LojaDB.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loja.Models;
+
+namespace Loja.Data
+{
+    public class LojaDB : DbContext
+    {
+        /// <summary>
+        /// Construtor da classe
+        /// serve para ligar esta classe à BD
+        /// </summary>
+        /// <param name="options"></param>
+        public LojaDB(DbContextOptions<LojaDB> options) : base(options) { }
+
+
+        // adicionar as 'tabelas' à BD
+        public DbSet<Order> Animais { get; set; }
+        public DbSet<Payment> Donos { get; set; }
+        public DbSet<Product> Veterinarios { get; set; }
+        public DbSet<Users> Consultas { get; set; }
+
+    }
+}
